65692f46094dc8148e71baa680d952fda270b779
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-05-08 16:28:10 +0300

    feat: add kebab case file

---
+++ src/scripts/others/toKebabCase.js
@@ -0,0 +1,11 @@
+const toKebabCase = (str) => {
+  if (str) {
+    return str
+      .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
+      .map((x) => x.toLowerCase())
+      .join('-');
+  }
+  return str;
+};
+
+export default toKebabCase;
