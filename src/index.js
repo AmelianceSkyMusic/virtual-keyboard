89000cdc47e33f9b3c9f99808f4f9d2b690bc9f5
Ameliance SkyMusic <amelianceskymusic@gmail.com>
2022-05-09 21:36:38 +0300

    refactor: add initHtml

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+import initHtml from './initHtml';
+
 import './style/scss/dark.scss';
 import './style/style.scss';
-
 import { generateHtml, sendTypeKeyToVirtualKeyboard } from './html-generator';
+
+initHtml('Virtual Keyboard', './assets/favicon.ico');
 
 generateHtml();
 
